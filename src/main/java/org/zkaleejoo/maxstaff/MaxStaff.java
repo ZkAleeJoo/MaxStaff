d966d408281d21e9f29f9866c14037e5e1656730
ZkAleeJoo <xxaleejooxx@gmail.com>
2025-12-13 12:32:23 -0500

    update

--- src/main/java/org/zkaleejoo/maxstaff/MaxStaff.java
+++ src/main/java/org/zkaleejoo/maxstaff/MaxStaff.java
@@ -1,30 +1,36 @@
 package org.zkaleejoo.maxstaff;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.zkaleejoo.maxstaff.commands.StaffCommand;
+import org.zkaleejoo.maxstaff.managers.StaffModeManager;
 
 public class MaxStaff extends JavaPlugin {
 
     private static MaxStaff instance;
+    private StaffModeManager staffModeManager;
 
     @Override
     public void onEnable() {
         instance = this;
         
-        // Mensaje en consola con colores para confirmar que cargó
-        getLogger().info("--------------------------------");
-        getLogger().info(" MaxStaff se ha habilitado correctamente.");
-        getLogger().info(" Version: " + getDescription().getVersion());
-        getLogger().info("--------------------------------");
-        
-        // Aquí registraremos comandos y eventos más tarde
+        this.staffModeManager = new StaffModeManager(this);
+
+
+        getCommand("staff").setExecutor(new StaffCommand(staffModeManager));
+
+        getLogger().info("MaxStaff habilitado correctamente (Modo Profesional).");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("MaxStaff se ha deshabilitado.");
+        getLogger().info("MaxStaff deshabilitado.");
     }
 
     public static MaxStaff getInstance() {
         return instance;
+    }
+    
+    public StaffModeManager getStaffModeManager() {
+        return staffModeManager;
     }
 }
