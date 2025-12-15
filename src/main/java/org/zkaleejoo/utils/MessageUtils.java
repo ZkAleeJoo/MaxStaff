728fce44d852644ec7b226ee5aebd0b4d07308ce
ZkAleeJoo <xxaleejooxx@gmail.com>
2025-12-14 21:44:07 -0500

    Update MessageUtils.java

--- src/main/java/org/zkaleejoo/utils/MessageUtils.java
+++ src/main/java/org/zkaleejoo/utils/MessageUtils.java
@@ -4,8 +4,10 @@ import org.bukkit.ChatColor;
 
 public class MessageUtils {
     
-        public static String getColoredMessage(String message) {
+    public static String getColoredMessage(String message) {
+        if (message == null) {
+            return ""; 
+        }
         return ChatColor.translateAlternateColorCodes('&', message);
     }
-
 }
