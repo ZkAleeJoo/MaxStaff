4ce40dbaf54d4a4588db14a3b25508104fffb06f
ZkAleeJoo <xxaleejooxx@gmail.com>
2025-12-15 10:43:39 -0500

    Create PunishmentCommand.java

---
+++ src/main/java/org/zkaleejoo/commands/PunishmentCommand.java
@@ -0,0 +1,74 @@
+package org.zkaleejoo.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.zkaleejoo.MaxStaff;
+import org.zkaleejoo.utils.MessageUtils;
+
+import java.util.Arrays;
+
+public class PunishmentCommand implements CommandExecutor {
+
+    private final MaxStaff plugin;
+
+    public PunishmentCommand(MaxStaff plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("maxstaff.punish")) {
+            sender.sendMessage(MessageUtils.getColoredMessage("&cNo tienes permiso."));
+            return true;
+        }
+
+        if (args.length < 1) {
+            sender.sendMessage(MessageUtils.getColoredMessage("&cUso: /" + label + " <jugador> [tiempo] [razón]"));
+            return true;
+        }
+
+        String target = args[0];
+        
+        // KICK
+        if (label.equalsIgnoreCase("kick")) {
+            String reason = args.length > 1 ? String.join(" ", Arrays.copyOfRange(args, 1, args.length)) : "Sin razón";
+            plugin.getPunishmentManager().kickPlayer(sender, target, reason);
+            return true;
+        }
+
+        // UNBAN / UNMUTE
+        if (label.equalsIgnoreCase("unban")) {
+            plugin.getPunishmentManager().unbanPlayer(sender, target);
+            return true;
+        }
+        if (label.equalsIgnoreCase("unmute")) {
+            plugin.getPunishmentManager().unmutePlayer(sender, target);
+            return true;
+        }
+
+        String time = "perm";
+        String reason = "Sin razón";
+        
+        int reasonStartIndex = 1;
+        if (args.length > 1) {
+            String arg1 = args[1].toLowerCase();
+            if (arg1.matches(".*[dhms]") || arg1.equals("perm")) {
+                time = arg1;
+                reasonStartIndex = 2;
+            }
+        }
+        
+        if (args.length > reasonStartIndex) {
+            reason = String.join(" ", Arrays.copyOfRange(args, reasonStartIndex, args.length));
+        }
+
+        if (label.equalsIgnoreCase("ban") || label.equalsIgnoreCase("tempban")) {
+            plugin.getPunishmentManager().banPlayer(sender, target, reason, time);
+        } else if (label.equalsIgnoreCase("mute") || label.equalsIgnoreCase("tempmute")) {
+            plugin.getPunishmentManager().mutePlayer(sender, target, reason, time);
+        }
+
+        return true;
+    }
+}
