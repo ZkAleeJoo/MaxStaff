60e177a6abb6b17f40b287b82683dfe54e7df211
ZkAleeJoo <xxaleejooxx@gmail.com>
2025-12-14 20:36:14 -0500

    Update MainConfigManager.java

--- src/main/java/org/zkaleejoo/config/MainConfigManager.java
+++ src/main/java/org/zkaleejoo/config/MainConfigManager.java
@@ -1,21 +1,34 @@
 package org.zkaleejoo.config;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.zkaleejoo.MaxStaff;
-
 
 public class MainConfigManager {
     
     private CustomConfig configFile;
+    private MaxStaff plugin;
+
+    private String prefix;
+    private String noPermission;
+    private String pluginReload;
+    private String subcommandInvalid;
+    private String subcommandSpecified;
 
     public MainConfigManager(MaxStaff plugin){
+        this.plugin = plugin;
         configFile = new CustomConfig("config.yml", null, plugin, false);
         configFile.registerConfig();
         loadConfig();
     }
 
-
     public void loadConfig(){
-   
+        FileConfiguration config = configFile.getConfig();
+        
+        prefix = config.getString("general.prefix");
+        noPermission = config.getString("messages.no-permission");
+        pluginReload = config.getString("messages.plugin-reload");
+        subcommandInvalid = config.getString("messages.subcommand-invalid");
+        subcommandSpecified = config.getString("messages.subcommand-specified");
     }
 
     public void reloadConfig(){
@@ -23,4 +36,9 @@ public class MainConfigManager {
         loadConfig();
     }
     
+    public String getPrefix() { return prefix; }
+    public String getNoPermission() { return noPermission; }
+    public String getPluginReload() { return pluginReload; }
+    public String getSubcommandInvalid() { return subcommandInvalid; }
+    public String getSubcommandSpecified() { return subcommandSpecified; }
 }
