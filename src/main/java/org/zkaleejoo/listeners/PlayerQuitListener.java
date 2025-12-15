0969ad0070e184a8f32826cbeb4b9e8490703f43
ZkAleeJoo <xxaleejooxx@gmail.com>
2025-12-14 22:05:39 -0500

    Create PlayerQuitListener.java

---
+++ src/main/java/org/zkaleejoo/listeners/PlayerQuitListener.java
@@ -0,0 +1,22 @@
+package org.zkaleejoo.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.zkaleejoo.MaxStaff;
+
+public class PlayerQuitListener implements Listener {
+
+    private final MaxStaff plugin;
+
+    public PlayerQuitListener(MaxStaff plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        if (plugin.getStaffManager().isInStaffMode(event.getPlayer())) {
+            plugin.getStaffManager().disableStaffMode(event.getPlayer());
+        }
+    }
+}
