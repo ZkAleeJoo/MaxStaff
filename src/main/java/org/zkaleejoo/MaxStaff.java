ad0d2e97a8a88426f703892d8f8c88ee51909d98
ZkAleeJoo <xxaleejooxx@gmail.com>
2025-12-29 08:53:35 -0500

    Backup

--- src/main/java/org/zkaleejoo/MaxStaff.java
+++ src/main/java/org/zkaleejoo/MaxStaff.java
@@ -1,108 +1,135 @@
 package org.zkaleejoo;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.zkaleejoo.commands.MainCommand;
-import org.zkaleejoo.commands.PunishmentCommand;
 import org.zkaleejoo.config.MainConfigManager;
-import org.zkaleejoo.listeners.*;
-import org.zkaleejoo.managers.*;
-import org.zkaleejoo.utils.MessageUtils;
-import org.zkaleejoo.utils.ServerVersion;
+
+import org.zkaleejoo.managers.StaffManager;
 import org.zkaleejoo.utils.UpdateChecker;
+import org.zkaleejoo.listeners.PlayerJoinListener;
+import org.zkaleejoo.listeners.StaffItemsListener;
+
+import org.zkaleejoo.listeners.PlayerQuitListener;
+
+import org.zkaleejoo.managers.GuiManager; 
+import org.zkaleejoo.listeners.GuiListener; 
+
+import org.zkaleejoo.managers.FreezeManager;
+import org.zkaleejoo.listeners.FreezeListener;
+
+import org.zkaleejoo.managers.PunishmentManager;
+import org.zkaleejoo.commands.PunishmentCommand;
+import org.zkaleejoo.listeners.ChatListener;
+
 
 public class MaxStaff extends JavaPlugin {
 
-    public static String prefix = "&8[&9MaxStaff&8] ";
-    private String version = getDescription().getVersion();
-    private String latestVersion;
-    private boolean isPaper;
-
     private MainConfigManager mainConfigManager;
     private StaffManager staffManager;
-    private PunishmentManager punishmentManager;
-    private FreezeManager freezeManager;
     private GuiManager guiManager;
+    private FreezeManager freezeManager;
+    private PunishmentManager punishmentManager;
+    private String latestVersion;
 
+    //PLUGIN SE PRENDE
     @Override
     public void onEnable() {
-        ServerVersion.getVersion(); 
-        checkServerType();
-
-        this.mainConfigManager = new MainConfigManager(this);
-        this.staffManager = new StaffManager(this);
-        this.punishmentManager = new PunishmentManager(this);
-        this.freezeManager = new FreezeManager(this);
-        this.guiManager = new GuiManager(this);
+        mainConfigManager = new MainConfigManager(this);
+        freezeManager = new FreezeManager(this);
+        staffManager = new StaffManager(this);
+        guiManager = new GuiManager(this);
+        punishmentManager = new PunishmentManager(this);
 
         registerCommands();
         registerEvents();
-
         checkUpdates();
 
-        sendConsoleHeader();
+        Bukkit.getConsoleSender().sendMessage("   _____                   _________ __          _____  _____ \r\n" + 
+                        "  /     \\ _____  ___  ___ /   _____//  |______ _/ ____\\/ ____\\\r\n" + 
+                        " /  \\ /  \\\\__  \\ \\  \\/  / \\_____  \\\\   __\\__  \\\\   __\\\\   __\\ \r\n" + 
+                        "/    Y    \\/ __ \\_>    <  /        \\|  |  / __ \\|  |   |  |   \r\n" + 
+                        "\\____|__  (____  /__/\\_ \\/_______  /|__| (____  /__|   |__|   \r\n" + 
+                        "        \\/     \\/      \\/        \\/           \\/              ");   
+
+        String prefix = mainConfigManager.getPrefix();
+        Bukkit.getConsoleSender().sendMessage(
+                ChatColor.translateAlternateColorCodes('&', prefix + "&fIt was activated correctly"));
+
+        
     }
 
+    private void checkUpdates() {
+    if (!getMainConfigManager().isUpdateCheckEnabled()) return;
+    new UpdateChecker(this, 130851).getVersion(version -> {
+        if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+            getLogger().info("You are using the latest version!");
+        } else {
+            this.latestVersion = version;
+            getLogger().warning("A new version is available: " + version);
+            getLogger().warning("Download it at: https://www.spigotmc.org/resources/130851/");
+        }
+        });
+    }
+
+    public String getLatestVersion() {
+    return latestVersion;
+    }
+
+    // PLUGIN SE APAGA
     @Override
     public void onDisable() {
-        Bukkit.getConsoleSender().sendMessage(MessageUtils.getColoredMessage(prefix + "&cPlugin successfully deactivated."));
-    }
 
-    private void checkServerType() {
-        try {
-            Class.forName("io.papermc.paper.event.packet.PlayerChunkLoadEvent");
-            this.isPaper = true;
-        } catch (ClassNotFoundException e) {
-            this.isPaper = false;
+        if (staffManager != null) {
+            staffManager.disableAllStaff();
         }
+
+        String prefix = (mainConfigManager != null) ? mainConfigManager.getPrefix() : "&4MaxStaff ";
+        Bukkit.getConsoleSender().sendMessage(
+            ChatColor.translateAlternateColorCodes('&', prefix + "&fIt was successfully deactivated"));
     }
 
     public void registerCommands() {
         MainCommand mainCommand = new MainCommand(this);
-        getCommand("maxstaff").setExecutor(mainCommand);
-        getCommand("maxstaff").setTabCompleter(mainCommand);
+        this.getCommand("maxstaff").setExecutor(mainCommand);
+        this.getCommand("maxstaff").setTabCompleter(mainCommand);
 
-        PunishmentCommand punishmentCommand = new PunishmentCommand(this);
-        getCommand("punish").setExecutor(punishmentCommand);
+        PunishmentCommand punCmd = new PunishmentCommand(this);
+        
+        String[] punCommands = {"ban", "tempban", "mute", "tempmute", "kick", "unban", "unmute", "warn"};
+        for (String cmd : punCommands) {
+            if (getCommand(cmd) != null) {
+                getCommand(cmd).setExecutor(punCmd);
+                getCommand(cmd).setTabCompleter(punCmd);
+            }
+        }
     }
+
+    public MainConfigManager getMainConfigManager() {
+        return mainConfigManager;
+    }
+
+    public StaffManager getStaffManager() {
+        return staffManager;
+    }
+
+    public GuiManager getGuiManager() {
+        return guiManager;
+    }
+
+    public FreezeManager getFreezeManager() {
+        return freezeManager;
+    }
+
+    public PunishmentManager getPunishmentManager() { return punishmentManager; }
 
     public void registerEvents() {
+        getServer().getPluginManager().registerEvents(new StaffItemsListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
-        getServer().getPluginManager().registerEvents(new ChatListener(this), this);
-        getServer().getPluginManager().registerEvents(new FreezeListener(this), this);
         getServer().getPluginManager().registerEvents(new GuiListener(this), this);
-        getServer().getPluginManager().registerEvents(new StaffItemsListener(this), this);
-    }
-
-    private void sendConsoleHeader() {
-        String serverType = isPaper ? "&bPaper" : "&eSpigot/Bukkit";
-        Bukkit.getConsoleSender().sendMessage(MessageUtils.getColoredMessage("   _____                   _________ __          _____  _____ \r\n" + //
-                        "  /     \\ _____  ___  ___ /   _____//  |______ _/ ____\\/ ____\\\r\n" + //
-                        " /  \\ /  \\\\__  \\ \\  \\/  / \\_____  \\\\   __\\__  \\\\   __\\\\   __\\ \r\n" + //
-                        "/    Y    \\/ __ \\_>    <  /        \\|  |  / __ \\|  |   |  |   \r\n" + //
-                        "\\____|__  (____  /__/\\_ \\/_______  /|__| (____  /__|   |__|   \r\n" + //
-                        "        \\/     \\/      \\/        \\/           \\/              "));
-        Bukkit.getConsoleSender().sendMessage(MessageUtils.getColoredMessage("&7Version: &f" + version));
-        Bukkit.getConsoleSender().sendMessage(MessageUtils.getColoredMessage("&7Server: " + serverType));
-        Bukkit.getConsoleSender().sendMessage(MessageUtils.getColoredMessage("&7Detected: &a" + ServerVersion.getVersion().name()));
-    }
-
-    public MainConfigManager getMainConfigManager() { return mainConfigManager; }
-    public StaffManager getStaffManager() { return staffManager; }
-    public PunishmentManager getPunishmentManager() { return punishmentManager; }
-    public FreezeManager getFreezeManager() { return freezeManager; }
-    public GuiManager getGuiManager() { return guiManager; }
-    public boolean isPaper() { return isPaper; }
-    public String getLatestVersion() { return latestVersion; }
-
-    private void checkUpdates() {
-        if (!mainConfigManager.isUpdateCheckEnabled()) return;
-        new UpdateChecker(this, 122239).getVersion(version -> {
-            if (!this.getDescription().getVersion().equalsIgnoreCase(version)) {
-                this.latestVersion = version;
-                getLogger().warning("New version available: " + version);
-            }
-        });
+        getServer().getPluginManager().registerEvents(new FreezeListener(this), this);
+        getServer().getPluginManager().registerEvents(new ChatListener(this), this);
     }
 }
--- src/main/java/org/zkaleejoo/utils/MessageUtils.java
+++ src/main/java/org/zkaleejoo/utils/MessageUtils.java
@@ -1,26 +1,13 @@
 package org.zkaleejoo.utils;
 
 import org.bukkit.ChatColor;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MessageUtils {
     
-    private static final Pattern HEX_PATTERN = Pattern.compile("&#([A-Fa-f0-9]{6})");
-
     public static String getColoredMessage(String message) {
-        if (message == null || message.isEmpty()) return "";
-
-        if (ServerVersion.isAtLeast(ServerVersion.v1_16_R1)) {
-            Matcher matcher = HEX_PATTERN.matcher(message);
-            StringBuilder sb = new StringBuilder();
-            while (matcher.find()) {
-                String color = matcher.group(1);
-                matcher.appendReplacement(sb, net.md_5.bungee.api.ChatColor.of("#" + color).toString());
-            }
-            message = matcher.appendTail(sb).toString();
+        if (message == null) {
+            return ""; 
         }
-
         return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
