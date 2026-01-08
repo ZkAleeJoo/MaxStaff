55c792439320785fffae4ac4563a4a9f1160f186
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-01-08 15:31:31 -0500

    Update V1_21_Handler.java

--- src/main/java/org/zkaleejoo/nms/versions/V1_21_Handler.java
+++ src/main/java/org/zkaleejoo/nms/versions/V1_21_Handler.java
@@ -3,8 +3,11 @@ package org.zkaleejoo.nms.versions;
 import org.zkaleejoo.nms.NMSHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.zkaleejoo.MaxStaff;
 
 public class V1_21_Handler implements NMSHandler {
+    private final MaxStaff plugin;
+    public V1_21_Handler(MaxStaff plugin) { this.plugin = plugin; }
 
     @Override
     public String getInventoryTitle(InventoryClickEvent event) {
@@ -13,6 +16,6 @@ public class V1_21_Handler implements NMSHandler {
 
     @Override
     public void hidePlayer(Player viewer, Player target) {
-        viewer.hidePlayer(org.zkaleejoo.MaxStaff.getPlugin(org.zkaleejoo.MaxStaff.class), target);
+        viewer.hidePlayer(plugin, target);
     }
 }
