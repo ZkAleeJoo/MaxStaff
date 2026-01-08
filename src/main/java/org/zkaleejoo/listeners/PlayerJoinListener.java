54ec5505dbf58bed33ec07a33f36a849b9fe9260
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-01-08 15:05:49 -0500

    Update PlayerJoinListener.java

--- src/main/java/org/zkaleejoo/listeners/PlayerJoinListener.java
+++ src/main/java/org/zkaleejoo/listeners/PlayerJoinListener.java
@@ -26,7 +26,7 @@ public class PlayerJoinListener implements Listener {
             Player staff = org.bukkit.Bukkit.getPlayer(uuid);
             if (staff != null && staff.isOnline()) {
                 if (!player.hasPermission("maxstaff.see.vanish")) {
-                    player.hidePlayer(plugin, staff);
+                    plugin.getNMS().hidePlayer(player, staff);
                 }
             }
         }
