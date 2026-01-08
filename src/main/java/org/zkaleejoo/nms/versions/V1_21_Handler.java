f65ba8dddb422da3214ce1287d2d2a60770924e0
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-01-08 15:01:29 -0500

    Create V1_21_Handler.java

---
+++ src/main/java/org/zkaleejoo/nms/versions/V1_21_Handler.java
@@ -0,0 +1,18 @@
+package org.zkaleejoo.nms.versions;
+
+import org.zkaleejoo.nms.NMSHandler;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class V1_21_Handler implements NMSHandler {
+    @Override
+    public String getInventoryTitle(InventoryClickEvent event) {
+        // En 1.21+ se usa el acceso directo al título del View
+        return event.getView().getTitle(); 
+    }
+
+    @Override
+    public void hidePlayer(Player viewer, Player target) {
+        viewer.hidePlayer(org.zkaleejoo.MaxStaff.getPlugin(org.zkaleejoo.MaxStaff.class), target);
+    }
+}
