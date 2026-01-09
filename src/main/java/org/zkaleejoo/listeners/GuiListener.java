f34655c5fd50a6d28cb0b34344bb17ef40f3ff5b
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-01-08 22:38:54 -0500

    Update GuiListener.java

--- src/main/java/org/zkaleejoo/listeners/GuiListener.java
+++ src/main/java/org/zkaleejoo/listeners/GuiListener.java
@@ -99,7 +99,7 @@ public class GuiListener implements Listener {
                 player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
                 Player targetPlayer = plugin.getServer().getPlayer(targetName);
                 if (targetPlayer != null) {
-                    plugin.getGuiManager().openUserInfoMenu(player, targetPlayer);
+                    plugin.getGuiManager().openSanctionMenu(player, targetName);
                 }
                 return;
             }
