bd2c57e7ea006afcbf2dca34e70b149af1d2f210
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-01-12 10:26:40 -0500

    Create CommandSpyCommand.java

---
+++ src/main/java/org/zkaleejoo/commands/CommandSpyCommand.java
@@ -0,0 +1,34 @@
+package org.zkaleejoo.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.zkaleejoo.MaxStaff;
+import org.zkaleejoo.utils.MessageUtils;
+
+public class CommandSpyCommand implements CommandExecutor {
+
+    private final MaxStaff plugin;
+
+    public CommandSpyCommand(MaxStaff plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + plugin.getMainConfigManager().getMsgConsole()));
+            return true;
+        }
+
+        Player player = (Player) sender;
+        if (!player.hasPermission("maxstaff.cmdspy")) {
+            player.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + plugin.getMainConfigManager().getNoPermission()));
+            return true;
+        }
+
+        plugin.getStaffManager().toggleCommandSpy(player);
+        return true;
+    }
+}
