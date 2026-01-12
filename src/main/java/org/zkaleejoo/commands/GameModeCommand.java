93ab0d82feebcd593fdbd1eaedcc810865034845
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-01-12 12:36:45 -0500

    Update GameModeCommand.java

--- src/main/java/org/zkaleejoo/commands/GameModeCommand.java
+++ src/main/java/org/zkaleejoo/commands/GameModeCommand.java
@@ -1,13 +1,19 @@
 package org.zkaleejoo.commands;
 
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter; 
 import org.bukkit.entity.Player;
 import org.zkaleejoo.MaxStaff;
 import org.zkaleejoo.utils.MessageUtils;
 
-public class GameModeCommand implements CommandExecutor {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GameModeCommand implements CommandExecutor, TabCompleter { 
 
     private final MaxStaff plugin;
 
@@ -23,13 +29,54 @@ public class GameModeCommand implements CommandExecutor {
         }
 
         Player player = (Player) sender;
-
         if (!player.hasPermission("maxstaff.gamemode")) {
             player.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + plugin.getMainConfigManager().getNoPermission()));
             return true;
         }
 
-        plugin.getGuiManager().openGameModeMenu(player);
+        if (args.length == 0) {
+            if (plugin.getMainConfigManager().isGmMenuEnabled()) {
+                plugin.getGuiManager().openGameModeMenu(player);
+            } else {
+                player.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + "&cUsage: /gm <0|1|2|3>"));
+            }
+            return true;
+        }
+
+        GameMode mode = matchGameMode(args[0]);
+        if (mode == null) {
+            player.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + "&cModo de juego inválido."));
+            return true;
+        }
+
+        player.setGameMode(mode);
+        
+        if (plugin.getStaffManager().isInStaffMode(player)) {
+            plugin.getStaffManager().updateSavedGameMode(player, mode);
+        }
+
+        player.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + 
+            plugin.getMainConfigManager().getGuiGmFeedback().replace("{mode}", mode.name())));
+        
         return true;
+    }
+
+    private GameMode matchGameMode(String arg) {
+        switch (arg.toLowerCase()) {
+            case "0": case "survival": case "s": return GameMode.SURVIVAL;
+            case "1": case "creative": case "c": return GameMode.CREATIVE;
+            case "2": case "adventure": case "a": return GameMode.ADVENTURE;
+            case "3": case "spectator": case "sp": return GameMode.SPECTATOR;
+            default: return null;
+        }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            return Arrays.asList("0", "1", "2", "3", "survival", "creative", "adventure", "spectator")
+                    .stream().filter(s -> s.startsWith(args[0].toLowerCase())).collect(Collectors.toList());
+        }
+        return null;
     }
 }
