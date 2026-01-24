73060ca0785bd344591bbc31b12efd2f70d87bdd
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-01-24 10:38:27 -0500

    Create FreezeCommand.java

---
+++ src/main/java/org/zkaleejoo/commands/FreezeCommand.java
@@ -0,0 +1,87 @@
+package org.zkaleejoo.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.zkaleejoo.MaxStaff;
+import org.zkaleejoo.utils.MessageUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FreezeCommand implements CommandExecutor, TabCompleter {
+
+    private final MaxStaff plugin;
+
+    public FreezeCommand(MaxStaff plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + plugin.getMainConfigManager().getMsgConsole()));
+            return true;
+        }
+
+        Player staff = (Player) sender;
+        String action = label.toLowerCase();
+
+        if (!staff.hasPermission("maxstaff.freeze")) {
+            staff.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + plugin.getMainConfigManager().getNoPermission()));
+            return true;
+        }
+
+        if (args.length < 1) {
+            staff.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + "&cUsage: /" + label + " <player>"));
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+
+        if (target == null) {
+            staff.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + plugin.getMainConfigManager().getMsgOffline()));
+            return true;
+        }
+
+        if (target.hasPermission("maxstaff.admin") || target.hasPermission("maxstaff.freeze")) {
+            staff.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + "&c¡Error! No puedes congelar a otro miembro del Staff."));
+            return true;
+        }
+
+        boolean isFrozen = plugin.getFreezeManager().isFrozen(target);
+
+        if (action.equals("freeze") || action.equals("ss")) {
+            if (isFrozen) {
+                staff.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + "&cEste jugador ya está congelado."));
+            } else {
+                plugin.getFreezeManager().setFrozen(target, true);
+                staff.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + plugin.getMainConfigManager().getMsgFreezeStaff().replace("{player}", target.getName())));
+            }
+        } else if (action.equals("unfreeze") || action.equals("uss")) {
+            if (!isFrozen) {
+                staff.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + "&cEste jugador no está congelado."));
+            } else {
+                plugin.getFreezeManager().setFrozen(target, false);
+                staff.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + plugin.getMainConfigManager().getMsgUnfreezeStaff().replace("{player}", target.getName())));
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            return Bukkit.getOnlinePlayers().stream()
+                    .map(Player::getName)
+                    .filter(name -> name.toLowerCase().startsWith(args[0].toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+        return new ArrayList<>();
+    }
+}
