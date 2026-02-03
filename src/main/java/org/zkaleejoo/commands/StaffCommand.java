0378e6bee3b617920958436e5b72cf179efce658
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-02-03 07:51:42 -0500

    Update StaffCommand.java

--- src/main/java/org/zkaleejoo/commands/StaffCommand.java
+++ src/main/java/org/zkaleejoo/commands/StaffCommand.java
@@ -5,7 +5,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.zkaleejoo.MaxStaff;
-import org.zkaleejoo.managers.StaffManager;
 import org.zkaleejoo.utils.MessageUtils;
 
 public class StaffCommand implements CommandExecutor {
@@ -19,20 +18,24 @@ public class StaffCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPrefix() + plugin.getMainConfigManager().getMsgConsole()));
+            sender.sendMessage(MessageUtils.getColoredMessage(
+                plugin.getMainConfigManager().getPrefix() + 
+                plugin.getMainConfigManager().getMsgConsole()
+            ));
             return true;
         }
 
         Player player = (Player) sender;
 
         if (!player.hasPermission("maxstaff.command.staff")) {
-            player.sendMessage(MessageUtils.getColoredMessage("&cNo tienes permiso para usar este comando."));
+            sender.sendMessage(MessageUtils.getColoredMessage(
+                plugin.getMainConfigManager().getPrefix() + 
+                plugin.getMainConfigManager().getNoPermission()
+            ));
             return true;
         }
 
         plugin.getStaffManager().toggleStaffMode(player); 
-        
-        //StaffManager.toggleStaffMode(player);
         
         return true;
     }
